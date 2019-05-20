3a3f06311afbd9df40799e0115bdbf875e3c6adc
justflying <jonglae@gmail.com>
2019-05-20 11:28:46 +0900

    First China Lotgen

--- app/src/main/java/gotopark/buster/chinaLotgen/MainActivity.java
+++ app/src/main/java/gotopark/buster/chinaLotgen/MainActivity.java
@@ -30,17 +30,13 @@ import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -49,12 +45,10 @@ import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
-
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-
 import gotopark.buster.chinaLotgen.Module.numtoimg;
 import gotopark.buster.chinaLotgen.Module.randomNum;
 import gotopark.buster.chinaLotgen.database.DatabaseHelper;
@@ -65,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
 
     public TextView text1;
     public TextView text10;
-
     private TextView Balltxt1;
     private TextView Balltxt2;
     private TextView Balltxt3;
@@ -135,15 +128,11 @@ public class MainActivity extends AppCompatActivity {
             LotCOPY();
             ctextR = ctextR + "\n" + "彩票日 : " + LotDate;
             ctextR = ctextR + "\n" + SUM_lotto_num + "\n" + getString(R.string.twiter_CH);
-
-
             String comText = Balltxt1.getText().toString();
 
             if (Objects.equals("0", comText)) {
-
                 text1.setText("首先开始生成数字！\n");
                 text1.append(getString(R.string.app_Das));
-
             } else {
                 BackProcessHandler.ClipsBoards();
 
@@ -171,13 +160,9 @@ public class MainActivity extends AppCompatActivity {
             String comText = getString(R.string.Main_text);
 
             if (Objects.equals(ctextR, comText)) {
-
                 text1.setText("首先开始生成数字！\n");
                 text1.append(getString(R.string.app_Das));
-
-
             } else {
-
                 ClipData clip = ClipData.newPlainText("复制", ctextR);
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 assert clipboard != null;
@@ -200,48 +185,32 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             soundpool.play(tak, 1, 1, 1, 0, 1);
 
-//            MultiClick = Model.getClick();
-
-//            Log.d("====MultiClick====", String.valueOf(MultiClick));
-
             if (MultiClick == 1) {
-                // 반복 회수 지정
-
                 int xnum = rand.nextInt(9);
                 millisec = Times_Ran[xnum];
             } else {
-
-
                 millisec = 50;
             }
 
-            // 반복 회수 끝
-
             CountDownTimer start = new CountDownTimer(millisec, 25) {
-
                 @SuppressLint("SetTextI18n")
                 public void onTick(long millisUntilFinished) {
                     soundpool.play(tak, 1, 1, 1, 0, 1);
                     text10.setText(" - 小数分析 - " + millisUntilFinished / 25 + "00ms 留。");
                     GenNumber();
                 }
-
                 @SuppressLint("SetTextI18n")
                 public void onFinish() {
-
                     text10.setText(getString(R.string.scr_text2));
                     count = count + 1;
-
                     if (count == primeWord) {
                         String saywords = BackProcessHandler.frontSay();
                         text1.setText(saywords);
                         count = 0;
                         primeWord = rand.nextInt(11) + 3;
-
                     }
                     ClickCount = 0;
                 }
-
             }.start();
         }
     };
@@ -262,14 +231,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchFullFragmentActivity(View v) {
         launchActivity(FullScannerFragmentActivity.class);
-
     }
-
-
     private void LotCOPY() {
         String App_links1 = getString(R.string.app_Google_Play_links);
         String App_Share = getString(R.string.app_share);
-
         //텍스트 변환
         String ctext1;
         String ctext2;
@@ -286,16 +251,10 @@ public class MainActivity extends AppCompatActivity {
         ctext5 = Balltxt5.getText().toString();
         ctext6 = Balltxt6.getText().toString();
         ctext7 = Balltxt7.getText().toString();
-
-
         ctextRlist = ctext1 + ", " + ctext2 + ", " + ctext3 + ", " + ctext4 + ", " + ctext5 + ", " + ctext6 +
                 "  (" + ctext7 + ")";
-
         ctextR = App_Share + ctextRlist + "\n" + App_links1;
     }
-
-
-    // MDCT  클릭시 브라우저 실행 url  이동하여 보여준다.
 
     @SuppressLint("CutPasteId")
     @Override
@@ -320,22 +279,18 @@ public class MainActivity extends AppCompatActivity {
 
         // wifi 또는 모바일 네트워크 어느 하나라도 연결이 되어있다면,
         if (wifi.isConnected() || mobile.isConnected()) {
-
             new LotonumCall().execute();
             Admob_is();
             Admob_Front();
             BackProcessHandler.AnRate();
 
-
         } else {
 
             AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
             {
-
                 ad.setTitle(getString(R.string.info_net1));
                 ad.setMessage(getString(R.string.info_net2));
                 ad.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         dialogInterface.dismiss();
@@ -368,14 +323,12 @@ public class MainActivity extends AppCompatActivity {
         Rtext7 = findViewById(R.id.Rtext7);
         Rtext8 = findViewById(R.id.Rtext8);
 
-
         Button btn1 = findViewById(R.id.button1);
         Button btn2 = findViewById(R.id.button2);
         Button btn3 = findViewById(R.id.button3);
         Button btn4 = findViewById(R.id.button4);
         Button btn5 = findViewById(R.id.button5);
         Button btn6 = findViewById(R.id.button6);
-
 
         //========================================================================
 
@@ -390,10 +343,8 @@ public class MainActivity extends AppCompatActivity {
         //========================================================================
 
         Rtilte = findViewById(R.id.rtitle);
-
         sw1 = findViewById(R.id.switch1);
         sw2 = findViewById(R.id.switch2);
-
         /*
          *
          * 시작시 메세지 출력
@@ -441,21 +392,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 soundpool.play(tak, 1, 1, 1, 0, 0);
                 if (sw1.isChecked()) {
-
                     randconut = 1;
-
-
                 } else {
-
                     randconut = 0;
-
-
                 }
-
             }
         });
-
-
     }
 
 
@@ -465,25 +407,16 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             soundpool.play(tok, 1, 1, 1, 0, 1);
-
             if (ClickCount == 0) {
-
                 LotCOPY();
-
                 db.insertColumn(ctextRlist, "");
-
                 ClickCount = 1;
-
                 String Mesg1 = "保存号码已完成";
-
                 //The number has been saved.
                 text10.setText(ctextRlist + " -> " + Mesg1);
                 Toast.makeText(MainActivity.this, Mesg1, Toast.LENGTH_SHORT).show();
-
             } else {
-
                 text10.setText("请创建号码。");
-
             }
         }
     };
@@ -494,10 +427,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             soundpool.play(tok, 1, 1, 1, 0, 1);
-
             Intent intent = new Intent(MainActivity.this, Main2Activity.class);
             startActivity(intent);
-
         }
     };
 
@@ -507,10 +438,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
     }
-
-
     public void Admob_is() {
-
         AdView mAdView = findViewById(R.id.adView);
         MobileAds.initialize(getApplicationContext(), getString(R.string.google_banner_id));
         AdRequest adRequest = new AdRequest.Builder().build();
@@ -522,28 +450,21 @@ public class MainActivity extends AppCompatActivity {
         Log.e("전면_Front_TEST =====> ", "=======================> OK");
         mInterstitialAd = new InterstitialAd(this);
         mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
-
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
-
         mInterstitialAd.loadAd(adRequest);
-
-
     }
 
     public void Show_front() {
         if (mInterstitialAd.isLoaded()) {
             mInterstitialAd.show();
         }
-
-
     }
 
 
     @Override
     public void onBackPressed() {
-
         BackProcessHandler.onBackPressed();
     }
 
@@ -569,13 +490,10 @@ public class MainActivity extends AppCompatActivity {
             res[5] = random.nextInt(33 - 31 + 1) + 31;
             blue_res = String.valueOf(random.nextInt(16 - 1 + 1) + 1);
 
-
         } else {
             res = Num.lotArray(6, 33);
             blue_res = String.valueOf(random.nextInt(16 - 1 + 1) + 1);
         }
-
-
         Balltxt1.setText(String.valueOf(res[0]));
         Balltxt2.setText(String.valueOf(res[1]));
         Balltxt3.setText(String.valueOf(res[2]));
@@ -583,14 +501,11 @@ public class MainActivity extends AppCompatActivity {
         Balltxt5.setText(String.valueOf(res[4]));
         Balltxt6.setText(String.valueOf(res[5]));
         Balltxt7.setText(blue_res);
-
-
     }
 
 
     @SuppressLint("StaticFieldLeak")
     public class LotonumCall extends AsyncTask<Void, Void, Void> {
-
 
         Elements F10;
         Elements F11;
@@ -715,14 +630,10 @@ public class MainActivity extends AppCompatActivity {
                 // 당첨 누적액
                 String nowsell_mony = F14.replaceAll("\\<.*?>", "");
                 String next1won = F15.replaceAll("\\<.*?>", "");
-
-
                 //Impresion
 
                 LotDate = F10_1;
-
                 Rtilte.setText(F10_1);
-
 
                 //당첨번호 표시
                 RBall1.setImageResource(R.drawable.ball3);
@@ -742,7 +653,6 @@ public class MainActivity extends AppCompatActivity {
                 Rtext7.setText("+");
                 Rtext8.setText(String.valueOf(aF11_1[7]));
 
-
                 SUM_lotto_num = aF11_1[1] + " ," +
                         aF11_1[2] + " ," +
                         aF11_1[3] + " ," +
@@ -750,7 +660,6 @@ public class MainActivity extends AppCompatActivity {
                         aF11_1[5] + " ," +
                         aF11_1[6] + "   (" +
                         aF11_1[7] + ")";
-
 
                 // 로또넘버 맞추기 위해서 모델로 셋
                 Model.setWeeknum(aF11_1);
@@ -779,28 +688,20 @@ public class MainActivity extends AppCompatActivity {
                 rowview14.setText(aF12_1[13]);
                 rowview15.setText(aF12_1[14]);
 
-
                 stackMony1.setText("本期销售额：" + nowsell_mony + "元");
                 stackMony2.setText("下期一等奖奖池累计金额：" + next1won + "元");
-
             } else {
-
                 /** 네트웍 품질 문제 발생시 메세지 출력 */
-
                 Rtilte.setText(getString(R.string.net_Info1));
                 stackMony1.setText(getString(R.string.net_Info2));
                 stackMony2.setText(getString(R.string.net_Info3));
-
             }
-
         }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-
     }
 
 }
